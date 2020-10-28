dd89d1a50fbf7640603605341f6577696b43b240
Martina <martina.pozzerle@libero.it>
2020-10-28 17:51:43 +0100

    #sistemati

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,28 +1,33 @@
-// //creo input per inserire la parola
-// var stringInput = prompt("inserisci una parola palindroma");
-// //creo la funzione che mi permette di generare l'inverso della parola inserita
-// function isPalindroma(stringInput){
-//   var parola = stringInput.split("");
-//   console.log(parola);
-//   parola.reverse();
-//   console.log(parola);
-//   return parola.join("");
-//   console.log(parola);
-// }
-// //creo le condizioni per cui la parola è polindorma o no. Dentro metto gli output
-// if (stringInput == isPalindroma(stringInput)){
-//   alert("La parola è palindroma!");
-// }else{
-//   alert("La parola non è palindroma!");
-// }
-//ALTRA VERSIONE ARRAY
+//creo input per inserire la parola
 var stringInput = prompt("inserisci una parola palindroma");
-function array(stringInput){
-  var newString = "";
-  for ( var i = -1; i >= stringInput.length; i--){
-  newString.push(stringInput[i]);
-  console.log(newString);
-}
-return newString;
 
+//creo le condizioni per cui la parola è polindorma o no. Dentro metto gli output
+if (stringInput == stringReverse2(stringInput)){
+  alert("La parola è palindroma!");
+}else{
+  alert("La parola non è palindroma!");
 }
+//creo la funzione che mi permette di generare l'inverso della parola inserita
+function stringReverse(stringToReverse){
+  var parola = stringToReverse.split("");
+  parola.reverse();
+  return parola.join("");
+}
+//creo la funzione che mi permette di generare l'inverso della parola inserita
+function stringReverse2(stringToReverse){
+  var newString = "";
+  for (var i = stringToReverse.length -1; i >= 0; i--){
+    newString += stringToReverse[i];
+  }
+  return newString;
+}
+//ALTRA VERSIONE ARRAY
+// var stringInput = prompt("inserisci una parola palindroma");
+// function array(stringInput){
+
+//   for ( var i = -1; i >= stringInput.length; i--){
+//   newString.push(stringInput[i]);
+//   console.log(newString);
+// }
+// return newString;
+// }
--- paridispari/script.js
+++ paridispari/script.js
@@ -1,5 +1,9 @@
 //voglio creare una checkbox che mi dia la scelta dell'utente tra pari e dispari; voglio un input text dove l'utente possa inserire il numero scelto; con un bottone voglio che i due valori (pari/dispari e numero) vengano presi; voglio poi un altro bottone con il quale si genera il numero random; faccio la somma tra il numero dell'utente e quello random; se il risultato è uguale a quello scelto dall'utente allora ha vinto, in caso contrario ha perso
 //creo le costanti che mi servono
+function randomBetween(max, min){
+  var random = Math.floor(Math.random() * (max - min) + min);
+  return random;
+}
 var evenOdd = document.getElementById("paridispari");
 var num = document.getElementById("numero");
 var MIN_NUM = 1;
@@ -8,38 +12,35 @@ var MAX_NUM = 5;
 var generate = document.getElementById("bottoneUt");
 generate.addEventListener("click", function(){
   evenOdd = document.getElementById("paridispari").value;
-  num = document.getElementById("numero").value;
+  num = parseInt(document.getElementById("numero").value);
   //stampo il numero dell'utente
   document.getElementById("numeroUt").innerHTML = num;
   if(num < MIN_NUM || num > MAX_NUM){
-    alert("Hai messo un numero troppo grande o troppo piccolo")
+    alert("Hai messo un numero troppo grande o troppo piccolo");
+    return;
   }
-  //con il click al secondo bottone creo un numero random
-  var randomnum = document.getElementById("bottoneRandom");
-  randomnum.addEventListener("click", function(){
-    var random = Math.floor(Math.random() * (MAX_NUM - MIN_NUM) + MIN_NUM);
-    //stampo il numero random
-    document.getElementById("rand").innerHTML = random;
-    //creo con una funzione il totale del numero random e del numero inserito dall'utente
-    function somma(num1, num2){
-      var addizione = num1 + num2;
-      return addizione;
-    }
-    var totale = somma(random, num);
-    var message = document.getElementById("result");
-    //creo le condizioni
-    if(totale % 2 == 0 && evenOdd == "pari"){
-      message.innerHTML = "Hai vinto!";
-    }
-    else if(totale % 2 == 1 && evenOdd == "dispari"){
 
-      message.innerHTML = "Hai vinto!";
-    }
-    else{
-      message.innerHTML = "Mi dispiace, hai perso";
-    }
-  })
+  var numRan = randomBetween(MAX_NUM, MIN_NUM);
+  //stampo il numero random
+
+  document.getElementById("rand").innerHTML = numRan;
+  //creo con una funzione il totale del numero random e del numero inserito dall'utente
+
+  var totale = numRan + num;
+  console.log(totale);
+  var messageEl = document.getElementById("result");
+  //creo le condizioni
+  if(totale % 2 == 0 && evenOdd == "pari"){
+    messageEl.innerHTML = "Hai vinto!";
+  }
+  else if(totale % 2 !== 0 && evenOdd == "dispari"){
+    messageEl.innerHTML = "Hai vinto!";
+  }
+  else{
+    messageEl.innerHTML = "Mi dispiace, hai perso";
+  }
 })
+
 
 
 // document.getElementById("rand").innerHTML = randomnum();
